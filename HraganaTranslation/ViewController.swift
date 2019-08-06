0716ffe678ad1565070fffd05c0095e8f84ce022
edasandesu <nowtimber@gmail.com>
2019-08-06 13:53:37 +0900

    Initial Commit

---
+++ HraganaTranslation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HraganaTranslation
+//
+//  Created by 今枝弘樹 on 2019/08/06.
+//  Copyright © 2019 Hiroki Imaeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
